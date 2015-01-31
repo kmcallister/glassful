f5412db0f5c109c1e2758fcb48b23eea261be491
Keegan McAllister <mcallister.keegan@gmail.com>
2015-01-31 13:51:01 -0800

    Add translator program

---
+++ src/bin/glassful.rs
@@ -0,0 +1,11 @@
+#![feature(io)]
+
+use std::old_io as io;
+
+extern crate glassful;
+
+pub fn main() {
+    let prog = io::stdin().read_to_end().unwrap();
+    let prog = String::from_utf8(prog).unwrap();
+    print!("{}", glassful::translate(prog));
+}
